86ca2ba588281cd8600c36ce56b0758b21e13515
Raul <123974722+3raulo9@users.noreply.github.com>
2024-01-01 19:44:21 +0200

    debuggin on

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
+from flask import Flask, render_template
 import sqlite3
 
+app = Flask(__name__)
 
-
-
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == "__main__":
-    pass
+    app.run(debug=True)
